d6a1504ff68f0a022cfc74bdcef4e962070f9a32
Twohy <twohy@ricks-macbook-pro.local>
2017-05-16 19:06:36 -0500

    Initial Commit

---
+++ 2etimeGraphics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  2etimeGraphics
+//
+//  Created by Twohy on 5/16/17.
+//  Copyright © 2017 2etime. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 2etimeGraphics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  2etimeGraphics
+//
+//  Created by Twohy on 5/16/17.
+//  Copyright © 2017 2etime. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
